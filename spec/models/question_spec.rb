3e4ba4b18a9b391ad3a152a0c4c82d3aebc46026
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-18 18:08:58 -0500

    Add valid test to question spec

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -5,6 +5,10 @@ describe Question do
     @question = Question.new(content: "What is the best Ruby tutorial?")
   end
 
+  it "should be valid when new" do 
+    @question.should be_valid
+  end
+
   it "should have attribute content" do
     expect(@question.content).to eq("What is the best Ruby tutorial?")
   end
