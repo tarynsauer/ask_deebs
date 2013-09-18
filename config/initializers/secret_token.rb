1f63ca8538ed8f323b0e39b23378995e82f0d540
dbc-apprentice <apprentice@dbc-chicago-008.local>
2013-09-18 16:51:21 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AskDeebs::Application.config.secret_key_base = 'f409edb4244d209a150bd45702986e78c6c500048d8fb271f3bfcff6e14f6e1694599ffbc96ec4d1a6f1b36640bbc06b013bdbfcee9246c01439f192bad2edce'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AskDeebs::Application.config.session_store :cookie_store, key: '_ask_deebs_session'
