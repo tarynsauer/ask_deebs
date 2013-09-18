a0a98ad702c02023d674951d2d8eea7a8f84327c
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-18 18:21:34 -0500

    Delete concerns folder

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130918231225) do
+ActiveRecord::Schema.define(version: 20130918231750) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,11 @@ ActiveRecord::Schema.define(version: 20130918231225) do
 
   create_table "questions", force: true do |t|
     t.text     "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "taggings", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
   end
