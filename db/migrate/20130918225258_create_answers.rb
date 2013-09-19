e8a220fc70537c72823150a70e5961e9be6ed932
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-19 13:29:52 -0500

    Add question_id column to answers migration

--- db/migrate/20130918225258_create_answers.rb
+++ db/migrate/20130918225258_create_answers.rb
@@ -2,6 +2,7 @@ class CreateAnswers < ActiveRecord::Migration
   def change
     create_table :answers do |t|
       t.text :content
+      t.belongs_to :question
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130919150358) do
+ActiveRecord::Schema.define(version: 20130919164103) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "answers", force: true do |t|
     t.text     "content"
+    t.integer  "question_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "question_followeds", force: true do |t|
+    t.integer  "question_id"
+    t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
