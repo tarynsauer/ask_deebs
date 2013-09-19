f40cdfc11140720760d97ce76458c091a554e839
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-19 12:15:15 -0500

    added question create logic and spec test

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -13,16 +13,19 @@ class QuestionsController < ApplicationController
   end
 
   def create
-    @question = Question.new(params[:question])
-
-    # Create callback in Question model to parse tags
-    p params[:question][:tags]
-
-    # @tags = params[:tags].map { |tag| tag.strip }
-    # @tags.each do |tag|
-    #   @question.tags.find_or_create_by(name: tag)
-    # end
-    # redirect_to @question
+    @question = Question.new(content: params[:question][:content])
+    
+    if @question.save
+      tags_array = params[:question][:tags].split(",")
+      @tags = tags_array.map { |tag| tag.strip }
+      
+      @tags.each do |tag|
+        @question.tags.find_or_create_by(name: tag)
+      end
+      redirect_to @question
+    else 
+      redirect_to new_question_path
+    end
   end
 
 
---
+++ spec/features/questions_integration_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+require 'capybara'
+
+describe 'New question form' do
+
+  # context "#create" do
+
+    it "should create a new question with tags" do
+      visit new_question_path
+      fill_in "question_content", :with => "What is the best JS tutorial?"
+      fill_in "question_tags", :with => "javascript, learning"
+      click_button 'Create Question'
+      page.should have_content("the best JS tutorial?")
+    end
+
+  # end
+
+end
