71f8bcb0ad99c18d18b64a0de91f129bf482609f
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-19 14:22:54 -0500

    start implementing user question creation

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -13,20 +13,22 @@ class QuestionsController < ApplicationController
   end
 
   def create
-    @question = Question.new(content: params[:question][:content])
-    
-    if @question.save
-      tags_array = params[:question][:tags].split(",")
-      @tags = tags_array.map { |tag| tag.strip }
-      
-      @tags.each do |tag|
-        @question.tags.find_or_create_by(name: tag)
+    if current_user
+      @user = current_user
+      @question = @user.questions.new(content: params[:question][:content])
+      if @question.save
+        tags_array = params[:question][:tags].split(",")
+        @tags = tags_array.map { |tag| tag.strip }
+        
+        @tags.each do |tag|
+          @question.tags.find_or_create_by(name: tag)
+        end
+        redirect_to @question
+      else
+        redirect_to new_question_path
       end
-      redirect_to @question
     else
-      redirect_to new_question_path
+      redirect_to signin_path
     end
   end
-
-
 end
