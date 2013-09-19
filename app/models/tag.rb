6ece9e1daa96276167978f24b955736bf170de2c
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-19 13:25:10 -0500

    Fix tagging validation

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,8 +1,5 @@
 class Tag < ActiveRecord::Base
 
-  validates :question_id, :presence => true
-  validates :tag_id, :presence => true
-
   has_many :taggings
   has_many :questions, through: :taggings
 end
--- app/models/tagging.rb
+++ app/models/tagging.rb
@@ -1,4 +1,7 @@
 class Tagging < ActiveRecord::Base
   belongs_to :tag
   belongs_to :question
+
+  validates :question_id, :presence => true
+  validates :tag_id, :presence => true
 end
