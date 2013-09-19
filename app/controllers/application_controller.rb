ad5a4ff7cc21d4061de7c4f449cd86001838b838
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-19 14:21:13 -0500

    moved helpers into application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,14 @@
 class ApplicationController < ActionController::Base
-  include ApplicationHelper
-  helper_method :authenticated?
+  
+  def authenticated?
+    !session[:oauth_token].nil?
+  end
+
+  def current_user
+    @current_user ||= User.new(session[:user_attributes])
+  end
+
+  helper_method: current_user 
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
