7851f468cf30397a8a18607b85d0a593bbce592f
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-18 21:46:08 -0500

    Work on question create action

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -13,7 +13,16 @@ class QuestionsController < ApplicationController
   end
 
   def create
-    
+    @question = Question.new(params[:question])
+
+    # Create callback in Question model to parse tags
+    p params[:question][:tags]
+
+    # @tags = params[:tags].map { |tag| tag.strip }
+    # @tags.each do |tag|
+    #   @question.tags.find_or_create_by(name: tag)
+    # end
+    # redirect_to @question
   end
 
 
