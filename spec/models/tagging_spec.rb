f1d1ac2f378c7022a247ddc2af8c095ce8ba16f9
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-18 20:27:25 -0500

    add taggings models, migration, specs

---
+++ spec/models/tagging_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe Tagging do
+  before(:each) do
+    @tagging = Tagging.new
+  end
+
+  it "should be valid when new" do
+    @tagging.should be_valid
+  end
+
+  it "should respond to question" do
+    @tagging.should respond_to(:question)
+  end
+
+  it "should respond to tag" do
+    @tagging.should respond_to(:tag)
+  end
+end
