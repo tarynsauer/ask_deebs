ca47af2407bbb5ad59fcf1bc7ea87e9b641540ec
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-19 12:38:31 -0500

    Start building answer form

---
+++ spec/controllers/questions_controller_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+describe QuestionsController do
+  
+  it "renders a new question form" do
+    pending
+  end
+
+  it "loads all questions into @questions" do
+    pending
+  end
+
+  
+end
