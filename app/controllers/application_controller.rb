4321632592b44efc2afd14b41c4e25b6cc7792ab
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-19 14:43:03 -0500

    Fix helper method for current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
     @current_user ||= User.new(session[:user_attributes])
   end
 
-  helper_method: current_user 
+  helper_method :current_user 
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
