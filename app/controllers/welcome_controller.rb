18095870151e3a42c0e2bb9dfdc1bf8e24ca3f87
Brian Tong <btong34@gmail.com>
2013-09-20 15:51:42 -0500

    hide answers if nothing searched

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,7 +3,11 @@ class WelcomeController < ApplicationController
   def index
     @user = current_user
     @questions = Question.search(params[:search])
-    @answers = Answer.search(params[:search])
+    if params[:search].blank?
+      @answers = nil
+    else
+      @answers = Answer.search(params[:search])
+    end
     @tags = Tag.order("taggings_count DESC")
   end
 end
