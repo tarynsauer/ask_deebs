94373c911c28ca5da145c5f96f34fb401e640d9c
dbc-apprentice <apprentice@dbc-chicago-008.local>
2013-09-19 21:08:35 -0500

    core voting functionality working.

---
+++ app/assets/javascripts/questions.js
@@ -0,0 +1,11 @@
+// $(document).ready(function () {
+//   $.post( "ajax/test.html", function( data ) {
+//     $( "a" ).click(function( event ) {
+//   event.preventDefault();
+//     $console.log(this);
+//     url = $()
+//     data =
+//   $( ".result" ).html( data );
+// });
+// });
+// });
