964e80739122e1d6fe609a977314442d5d4eb00a
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-20 16:11:26 -0500

    Remove oauth tests, fix home page test

--- spec/features/user_integration_spec.rb
+++ spec/features/user_integration_spec.rb
@@ -5,7 +5,7 @@ describe "the signin process", :type => :feature do
 
   it "finds the Home screen" do
     visit '/'
-    expect(page).to have_content 'Hello world'
+    expect(page).to have_content 'Tags'
   end
 
 end
