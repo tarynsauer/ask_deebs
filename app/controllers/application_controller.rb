3814b96b96953a8c56c0133e0d25e7d9fc8f4b53
Kelmer Perez <kelmerp@gmail.com>
2013-09-19 19:21:49 -0500

    changed authenticated? helper method to logged_in?

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < ActionController::Base
   
   
-  def authenticated?
-    !session[:oauth_token].nil?
+  def logged_in?
+    !current_user.nil?
   end
 
   def current_user
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
   end
 
   helper_method :current_user 
-  helper_method :authenticated?
+  helper_method :logged_in?
 
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
