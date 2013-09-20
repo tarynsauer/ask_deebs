4b34fefa0cdba8277607f06568832b038fb2ae4f
Taryn Sauer <taryn.sauer@gmail.com>
2013-09-20 18:21:12 -0500

    Add user_id to voting up and down functions

--- app/assets/javascripts/questions.js
+++ app/assets/javascripts/questions.js
@@ -1,11 +1,18 @@
-// $(document).ready(function () {
-//   $.post( "ajax/test.html", function( data ) {
-//     $( "a" ).click(function( event ) {
-//   event.preventDefault();
-//     $console.log(this);
-//     url = $()
-//     data =
-//   $( ".result" ).html( data );
-// });
-// });
-// });
+$(document).ready(function () {
+    $( ".vote_down" ).click(function( event ) {
+        var questionId = $(this).attr( "data-question" );
+        var answerId = $(this).attr( "id" );
+        var url = '/vote_down';
+        var data = { answer_id: answerId, like: false, question_id: questionId};
+            console.log(data);
+            $.post( url, data, function( response ) {
+              console.log(response);
+              // $(this).( ".count" ).html( data );
+            });
+
+
+    });
+});
+
+        // <span class="vote_up" data-question=<%="#{@question.id}"%> id=<%="#{answer.id}"%>>Vote Up</span>
+        // <span class="vote_down" data-question=<%="#{@question.id}"%> id=<%="#{answer.id}"%>>Vote Down</span>
--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -5,7 +5,7 @@ class AnswersController < ApplicationController
       @question = Question.find(params[:question_id])
       @answer = @question.answers.create(content: params[:answer][:content])
       @answer.update(user_id: current_user.id)
-  
+
       redirect_to @question
     else
       redirect_to signin_path
@@ -14,15 +14,31 @@ class AnswersController < ApplicationController
   end
 
   def vote_up
-    vote = Vote.find_or_create_by_user_id_and_answer_id(answer_id: params[:answer_id], like: params[:like])
+    vote = Vote.find_or_create_by_user_id_and_answer_id(user_id: session[:user_id], answer_id: params[:answer_id], like: params[:like])
     vote.update(like: true)
-    redirect_to question_path(params[:question_id])
+
+    if request.xhr?
+
+      vote_count = Answer.find(params[:answer_id]).votes.count
+      content_type :json
+      { answer_id: params[:answer_id], count: vote_count }.to_json
+    else
+      redirect_to question_path(params[:question_id])
+    end
   end
 
   def vote_down
-    vote = Vote.find_or_create_by_user_id_and_answer_id(answer_id: params[:answer_id], like: params[:like])
+    vote = Vote.find_or_create_by_user_id_and_answer_id(user_id: session[:user_id], answer_id: params[:answer_id], like: params[:like])
     vote.update(like: false)
-    redirect_to question_path(params[:question_id])
+
+    if request.xhr?
+
+      vote_count = Answer.find(params[:answer_id]).votes.count
+      content_type :json
+      { answer_id: params[:answer_id], count: vote_count }.to_json
+    else
+      redirect_to question_path(params[:question_id])
+    end
   end
 
 end
