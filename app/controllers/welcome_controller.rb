a6f7d683b1e7fc7c8b8657da17b8da4156dac3f7
Brian Tong <btong34@gmail.com>
2013-09-21 13:01:17 -0500

    include answers to live search results

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,22 +4,16 @@ class WelcomeController < ApplicationController
     @user = current_user
     
     if request.xhr?
+      #show all questions on page load
       @questions = Question.search(params[:search])
+
+      #only show answers if searched for
+      params[:search].blank? ? @answers = nil : @answers = Answer.search(params[:search])
+      
       render :partial => "live_search", :layout => false
     else
       @questions = Question.all
     end
-
-    #show all questions even without searching
-    # @questions = Question.search(params[:search])
-
-    # #don't show answers unless searched for
-    # if params[:search].blank?
-    #   @answers = nil
-    # else
-    #   @answers = Answer.search(params[:search])
-    # end
-    
     @tags = Tag.order("taggings_count DESC")
   end
 end
