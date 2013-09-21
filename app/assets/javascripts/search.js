41658c24d07ea8eb602240695c4238af98ded90e
Brian Tong <btong34@gmail.com>
2013-09-21 12:38:55 -0500

    Add live search functionality

---
+++ app/assets/javascripts/search.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $("#search").bind("keyup", function() {
+        var url = $("#search_form").attr("action")
+        var data = $("#search_form").serialize();
+
+        $.get(url, data, function(response){
+
+            $("#live-search").html(response);
+        });
+    });
+})
+
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,12 +2,24 @@ class WelcomeController < ApplicationController
 
   def index
     @user = current_user
-    @questions = Question.search(params[:search])
-    if params[:search].blank?
-      @answers = nil
+    
+    if request.xhr?
+      @questions = Question.search(params[:search])
+      render :partial => "live_search", :layout => false
     else
-      @answers = Answer.search(params[:search])
+      @questions = Question.all
     end
+
+    #show all questions even without searching
+    # @questions = Question.search(params[:search])
+
+    # #don't show answers unless searched for
+    # if params[:search].blank?
+    #   @answers = nil
+    # else
+    #   @answers = Answer.search(params[:search])
+    # end
+    
     @tags = Tag.order("taggings_count DESC")
   end
 end
