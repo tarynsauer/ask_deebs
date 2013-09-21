fa80230f1917cf3689ac08f0632bc0e3b070f971
dbc-apprentice <apprentice@dbc-chicago-008.local>
2013-09-21 12:13:31 -0500

    removed console.log messages and fixed whitespace and indentation

--- app/assets/javascripts/questions.js
+++ app/assets/javascripts/questions.js
@@ -5,9 +5,7 @@ $(document).ready(function () {
       var questionId = $(this).attr( "data_question" );
       var answerId = $(this).attr( "id" );
       var data = { answer_id: answerId, like: 'false', question_id: questionId };
-          console.log(data)
           $.get( '/vote', data, function( response ) {
-            console.log(response)
             $('.count-' + response.answer_id).html('LIKES: ' + response.count);
           });
   });
@@ -18,35 +16,30 @@ $(document).ready(function () {
         var questionId = $(this).attr( "data_question" );
         var answerId = $(this).attr( "id" );
         var data = { answer_id: answerId, like: 'true', question_id: questionId };
-                console.log(data);
             $.get( '/vote', data, function( response ) {
-              console.log(response);
               $('.count-' + response.answer_id).html('LIKES: ' + response.count);
-            });
-
-
+        });
     });
 });
 
 
 $(document).ready(function(){
-  $('h3.ask-question').on("click", function(event){
-    event.preventDefault();
-    $.get("/questions/new", function(data) {
-      $('h3.ask-question').hide();
-      $('form.search').before(data);
+    $('h3.ask-question').on("click", function(event){
+        event.preventDefault();
+        $.get("/questions/new", function(data) {
+            $('h3.ask-question').hide();
+            $('form.search').before(data);
+        });
     });
-  });
 
-  $('form#new_answer').submit(function(event){
-    event.preventDefault();
-    formContent = $('textarea#answer_content').val();
-    data = { answer : { content : formContent } }
-    formUrl = $('form#new_answer').attr('action');
-    $.post(formUrl, data, function(success){
-      console.log(success);
-      $('ul.answers').prepend(success);
-    }, "html");
-  });
+    $('form#new_answer').submit(function(event){
+        event.preventDefault();
+        formContent = $('textarea#answer_content').val();
+        data = { answer : { content : formContent } }
+        formUrl = $('form#new_answer').attr('action');
+        $.post(formUrl, data, function(success){
+            $('ul.answers').prepend(success);
+        }, "html");
+    });
 });
 
